443fcb6e350e823731ef58b09c1340ad7df973ce
JasonArrechea <166848585+JasonArrechea@users.noreply.github.com>
2025-04-24 10:30:16 -0500

    Update GuiaDetalle.jsx

--- turismo-frontend/src/pages/GuiaDetalle.jsx
+++ turismo-frontend/src/pages/GuiaDetalle.jsx
@@ -1,4 +1,3 @@
-// src/pages/GuiaDetalle.jsx
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import SpinnerCentered from "../components/UI/SpinnerCentered";
